682b69228c2b50cb290dc2c4a676cfdfc0f4f0f5
misaken <52680194+misaken@users.noreply.github.com>
2023-10-06 10:29:35 +0900

    スクレイピング

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #main.py
+from bs4 import BeautifulSoup
+import requests
+
+query = "mars weather"
+
+url = "https://www.google.com/search?q=" + query
+url = "http://www.google.com/"
+url = "https://data.nasa.gov/"
+res = requests.get(url)
+
+soup = BeautifulSoup(res.text, "html.parser")
+
+print(soup)
+search_input = soup.find("input", {"name": "q"})
+search_input["value"] = query
+print(soup.prettify)
